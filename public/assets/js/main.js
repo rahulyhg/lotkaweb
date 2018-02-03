2d3195fc592e63f3602f03cc3941bf1b03d61eb5
pjotor <github@pjotor.com>
2018-02-03 05:41:53 -0500

    AA: adding global .autoExpand functionality to input fields.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -903,6 +903,21 @@
         }
 
     });
+  
+  $(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function() {
+      var savedValue = this.value;
+      this.value = '';
+      this.baseScrollHeight = this.scrollHeight;
+      this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function() {
+      var minRows = this.getAttribute('data-min-rows') | 0,
+        rows;
+      this.rows = minRows;
+      rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
+      this.rows = minRows + rows;
+    }); 
 
 })(jQuery);
 
