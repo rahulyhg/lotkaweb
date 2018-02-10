120b4306f720967b4c14a3092715390d129bbd5a
pjotor <github@pjotor.com>
2018-02-10 06:42:14 -0500

    One more time, with feeling.

--- src/app/Controllers/Page/Participant/CharacterPageController.php
+++ src/app/Controllers/Page/Participant/CharacterPageController.php
@@ -124,7 +124,7 @@ class CharacterPageController extends Controller
       self::setAttribute($character, $key, $value);
     }
 
-    $this->flash->addMessage('debug', [$user->displayname]);
+    $this->flash->addMessage('debug', [$user->displayname, self::mapAttributes($user->fresh()->attr)]);
     
     # Check if we have updated data
     $hasUpload = $request->getUploadedFiles();
