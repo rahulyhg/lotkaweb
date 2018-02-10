27b2afc0d575561cce4858ad4d03d4088445ba22
pjotor <github@pjotor.com>
2018-02-10 06:33:07 -0500

    How about I go out and get drunk instead?

--- src/app/Controllers/Page/Participant/CharacterPageController.php
+++ src/app/Controllers/Page/Participant/CharacterPageController.php
@@ -124,7 +124,7 @@ class CharacterPageController extends Controller
       self::setAttribute($character, $key, $value);
     }
 
-    $this->flash->addMessage('debug', [$user->displayname, $debug]);
+    $this->flash->addMessage('debug', [$user->displayname, self:mapAttributes($user->attr)]);
     
     # Check if we have updated data
     $hasUpload = $request->getUploadedFiles();
