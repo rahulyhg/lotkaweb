f011c13c98526c4b5cc01ebdeee8237cba184f6f
pjotor <github@pjotor.com>
2018-02-10 05:55:59 -0500

    Stumped by the debugger...

--- src/app/Controllers/Page/Participant/CharacterPageController.php
+++ src/app/Controllers/Page/Participant/CharacterPageController.php
@@ -124,7 +124,7 @@ class CharacterPageController extends Controller
       self::setAttribute($character, $key, $value);
     }
 
-    $this->flash->addMessage('debug', [$debug, $user_attributes]);
+    $this->flash->addMessage('debug', $debug);
     
     # Check if we have updated data
     $hasUpload = $request->getUploadedFiles();
