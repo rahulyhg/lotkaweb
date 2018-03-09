ce68ecffa4a46984522bbb32149be701cbacd549
tz <kreuzweg@gmail.com>
2018-03-09 15:35:30 +0100

    Fixes to shop

--- public/shop.php
+++ public/shop.php
@@ -87,6 +87,7 @@ $(document).ready(function() {
 <img src="https://lotka-volterra.se/assets/media/f50f3d92f5923c50.jpg"><br>
 <h1>Bus ticket (Airport)</h1>
 <p>Return ticket between Arlanda Airport and the larp venue. Leaves from Arlanda Airport at 11:30 on thursday and leaves for the airport on sunday at 11:00.</p>
+<p><b>NOTE!</b> Deadline for ordering this item is March 25th</p>
 <button type="button" class="addToCart btn btn-green" data-id="bus_airport"><span class="fa"></span>SEK 200</button>
 <input type="hidden" name="bus_airport" value="0">
     </td>
@@ -94,15 +95,17 @@ $(document).ready(function() {
 <img src="https://lotka-volterra.se/assets/media/3283433f2194fd9f.jpg"><br>
 <h1>Bus ticket (Stockholm Central)</h1>
 <p>Return ticket between Stockholm Central and the larp venue. Leaves from Stockholm Central at 10:50 on thursday and leaves for the Central on sunday at 11:00.</p>
+<p><b>NOTE!</b> Deadline for ordering this item is March 25th</p>
 <button type="button" class="addToCart btn btn-green" data-id="bus_central"><span class="fa"></span>SEK 200</button>
 <input type="hidden" name="bus_central" value="0">
     </td>
   </tr>
   <tr>
     <td>
-<img src="foo"><br>
-<h1>T-shirt ("Gents" fit)</h1>
-<p>Swag to show you were there! Tshirt is anthracite grey with three-color print. <i>"Gents" is what the tshirt maker call this model, not our term</i>.</p>
+<img src="https://lotka-volterra.se/assets/media/a52442aeff968024.jpg"><br>
+<h1>T-shirt ("Gents" model)</h1>
+<p>Swag to show you were there! Dark tshirt with color print. <i>"Gents" is what the tshirt maker call this model, not our term</i>.</p>
+<p><b>NOTE!</b> Deadline for ordering this item is March 25th</p>
 <select name="gents_tshirt_size">
   <option value="S">Small</option>
   <option value="M">Medium</option>
@@ -111,13 +114,14 @@ $(document).ready(function() {
   <option value="XXL">XX-Large</option>
   <option value="XXXL">XXX-Large</option>
 </select><br>
-<button type="button" class="addToCart btn btn-green" data-id="gents_tshirt"><span class="fa"></span>SEK 200</button>
+<button type="button" class="addToCart btn btn-green" data-id="gents_tshirt"><span class="fa"></span>SEK 250</button>
 <input type="hidden" name="gents_tshirt" value="0">
     </td>
     <td>
-<img src="foo"><br>
+<img src="https://lotka-volterra.se/assets/media/a52442aeff968024.jpg"><br>
 <h1>T-shirt ("Ladies" model)</h1>
-<p>Swag to show you were there! Tshirt is anthracite grey with three-color print. <i>"Ladies" is what the tshirt maker call this model, not our term</i>.</p>
+<p>Swag to show you were there! Dark tshirt with color print. <i>"Ladies" is what the tshirt maker call this model, not our term</i>.</p>
+<p><b>NOTE!</b> Deadline for ordering this item is March 25th</p>
 <select name="ladies_tshirt_size">
   <option value="S">Small</option>
   <option value="M">Medium</option>
@@ -125,7 +129,7 @@ $(document).ready(function() {
   <option value="XL">X-Large</option>
   <option value="XXL">XX-Large</option>
 </select><br>
-<button type="button" class="addToCart btn btn-green" data-id="ladies_tshirt"><span class="fa"></span>SEK 200</button>
+<button type="button" class="addToCart btn btn-green" data-id="ladies_tshirt"><span class="fa"></span>SEK 250</button>
 <input type="hidden" name="ladies_tshirt" value="0">
     </td>
   </tr>
@@ -138,6 +142,11 @@ $(document).ready(function() {
 <input type="hidden" name="roll_mat" value="0">
     </td>
     <td>
+<img src="https://lotka-volterra.se/assets/media/d80abd3587a65eaf.jpg"><br>
+<h1>Camping cot</h1>
+<p>Want to keep your cot after the larp? Get it at a killer price.</p>
+<button type="button" class="addToCart btn btn-green" data-id="cot"><span class="fa"></span>SEK 150</button>
+<input type="hidden" name="cot" value="0">
     </td>
   </tr>
 </table>
