57a1d1ca80f8874523ae81432460423fb315cf26
pjotor <github@pjotor.com>
2018-03-08 20:41:55 -0500

    Adding a scaled down wersion of the list system

--- src/app/Controllers/Page/Participant/ParticipantPageController.php
+++ src/app/Controllers/Page/Participant/ParticipantPageController.php
@@ -84,5 +84,88 @@ class ParticipantPageController extends Controller
       'post' => $post,
       'current' => $participant
     ]);
-  } 
+  }
+  
+  private function packing_lists()
+  {
+    return [
+        "must_have" => [
+          "title" => "Items you need to bring",
+          "items" => [
+            "sleeping_bag" =>      "Sleeping Bag",
+            "sleeping_mat" =>      "Sleeping Mat",
+            "hygiene_articles" =>  "Hygiene Articles",
+            "boots" =>             "Boots",
+            "pants" =>             "Pants/trousers, preferrably with leg-pockets like cargo pants",
+            "socks" =>             "Socks (bring extra pairs)",
+            "sweater" =>           "Warm, neutral non-patterned sweater (knitted or similar)",
+            "gloves" =>            "Work gloves",
+            "underwear" =>         "Underwear",
+            "tshirt" =>            "Neutral t-shirt or tank top",
+            "undergarments" =>     "Warm undergarments (especially if you think you might end up topside).",
+            "self_care" =>         "Basic self-care kit (aspirin, blister band aids for your feet)"
+          ],
+        ],
+
+        "nice_to_have" => [
+          "title" => "Nice to have items",
+          "items" => [
+            "memorabilia" =>      "Memorabilia",
+            "earlpugs" =>         "Earplugs",
+            "cash" =>             "Cash for the bar on saturday (sek or euro)",
+            "snacks" =>           "Snacks and or food for thursday. It will be a long day.",
+            "flashlight" =>       "Torch/Flashlight",
+          ],
+        ],
+    ];
+  }
+
+  public function packing($request, $response, $arguments) 
+  {
+    $player = $this->container->auth->isWriter() && isset($arguments["uid"]) &&  $arguments["uid"] ?
+      self::getPlayerInfo($arguments["uid"]) : self::getCurrentUser();
+    $user = $player["user"];
+    
+    return self::render(
+      "packing-list", 
+      [
+        "user" => $user,
+        "packing_lists" => self::packing_lists(),
+      ], 
+      $response
+    );
+  }
+    
+  public function save_packing($request, $response, $arguments) 
+  {
+    $player = $this->container->auth->isWriter() && isset($arguments["uid"]) && $arguments["uid"] ?
+      self::getPlayerInfo($arguments["uid"]) : self::getCurrentUser();
+    $user = $player["user"];
+    
+    $packing_lists = self::packing_lists();
+    
+    $packing_attributes = array_merge(
+      array_keys($packing_lists["must_have"]["items"]),
+      array_keys($packing_lists["nice_to_have"]["items"]),
+      ["pnqs", "ta"]
+    );
+    
+    foreach($packing_attributes as $key) {
+      $value = $request->getParam($key);
+      $value = is_null($value) ? false : $value;
+      $debug[] = self::setAttribute($user, "packing_$key", $value);
+    }
+    
+    if($user->save()) {
+      $this->flash->addMessage('success', "Packing list updated."); 
+    } else {
+      $this->flash->addMessage('error', "Something went wrong saving the packing list, sorry."); 
+    }
+
+    return $response->withRedirect(
+      isset($arguments["uid"])  ? 
+      $this->router->pathFor('participant.packing', ['uid' => $arguments["uid"]]):
+      $this->router->pathFor('participant.packing')
+    );
+  }  
 }
