e8451d94fb253ba6c9a356d0a1c6f2d48561e5ba
Sebastian Utbult <kreuzweg@gmail.com>
2018-03-09 11:57:27 +0100

    Shop (#16)
    * Synching menu
    
    * Add shop to menu
    
    * Shop code

---
+++ public/thanks.php
@@ -0,0 +1,35 @@
+<?php
+ini_set("error_reporting", ~E_NOTICE);
+?><html>
+<head>
+    <link href="/assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="/assets/lib/animate.css/animate.css" rel="stylesheet">
+    <link href="/assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
+    <link href="/assets/lib/flexslider/flexslider.css" rel="stylesheet">
+    <link href="/assets/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="/assets/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
+    <link href="/assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
+    <link href="/assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+    <link href="/assets/lib/jquery.growl/stylesheets/jquery.growl.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pathway+Gothic+One" rel="stylesheet">
+    <link href="/assets/css/theme/lotka-volterra.css" rel="stylesheet">
+    <script src="/assets/lib/jquery/dist/jquery.js"></script>
+    <script src="/assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/assets/js/main.js"></script>
+<script src="/assets/js/theme/script.js"></script>
+</head>
+<body>
+<style>
+body {
+  padding:10px;
+}
+</style>
+
+<h1>Lotka-Volterra Shop</h1>
+
+<p>Your order is complete! Thank you.</p<
